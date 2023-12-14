184f35152401ac693f9ea63270154019f664eaa2
ismael <babalolaismael@gmail.com>
2023-12-14 15:11:19 -0800

    rewritten

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print a c program using printf*
+ *
+ * Return: 1
+ *
+ */
+
+
+
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
