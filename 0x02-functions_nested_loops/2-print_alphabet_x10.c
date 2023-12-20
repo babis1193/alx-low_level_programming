71c00e4ca99fbea09233d7d5debe357272934cf8
ismael <babalolaismael@gmail.com>
2023-12-19 20:21:37 -0800

    nested loop commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+		while (i <= 9)
+		{
+		for (alpha = 'a'; alpha <= 'z'; calpha+)
+		{
+			_putchar(alpha);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
